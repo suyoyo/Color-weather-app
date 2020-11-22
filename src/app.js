b1c9c4e805775651e66723c3d6c627fae5acab4d
suyoyo <yoyoplanet2000@gmail.com>
2020-11-22 17:25:43 +0100

    implemented the units conversion

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ function displayTemperature(response){
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt",response.data.weather[0].description);
+
+    celsiusTemperature = response.data.main.temp
 }
 
 function search(city){
@@ -43,8 +45,34 @@ function handleSubmit(event){
   search(cityInputElement.value)
 }
 
-search("Taipei");
+function displayFahrenheitTemperature(event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("inactiv");
+    fahrenheitLink.classList.add("inactiv");
+    let fahrenheitTemperature = (celsiusTemperature * 9)/5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.add("inactiv");
+    fahrenheitLink.classList.remove("inactiv");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+
+let celsiusTemperature = null;
 
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Taipei");
