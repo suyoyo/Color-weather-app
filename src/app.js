0671248b66e395e883d7e4daf576bafb0cc9adf1
suyoyo <yoyoplanet2000@gmail.com>
2020-11-22 15:47:18 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -31,10 +31,20 @@ function displayTemperature(response){
     iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
+function search(city){
+  let apiKey = "936a50f4c5816a4cf5f3c262e7a4d70f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value)
+}
+
+search("Taipei");
 
 
-let apiKey = "936a50f4c5816a4cf5f3c262e7a4d70f";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
